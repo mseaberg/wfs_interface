ee1a3c7987e5e6f891c78e35b769e60a4344ba40
Matthew H. Seaberg <seaberg@slac.stanford.edu>
2020-05-06 11:09:40 -0700

    troubleshooting scripts

--- start_reply.sh
+++ start_reply.sh
@@ -2,8 +2,8 @@
 source /reg/g/psdm/etc/psconda.sh
 echo $(hostname)
 #ssh psana 'cd wfs_v4; echo $(hostname); mpirun -n 8 python mpi_socket.py -r $1 -s $2' &
-command="source /reg/neh/home/seaberg/psana_setup.sh;"
-command+="cd /reg/neh/home/seaberg/Python/WFS_interface;"
-command+="bsub -q psfehq -n 16 -o log/%J.log mpirun python mpi_reply.py -b $1 -e $2 -r $3 -s $4 -c $5"
-ssh psana command &
-#ssh psana "source /reg/g/psdm/etc/psconda.sh; cd wfs_v4; bsub -q psfehq -n 16 -o log/%J.log mpirun python mpi_reply.py -b $1 -e $2 -r $3 -s $4 -c $5" &
+#command="source /reg/neh/home/seaberg/psana_setup.sh;"
+#command+="cd /reg/neh/home/seaberg/Python/WFS_interface;"
+#command+="bsub -q psfehq -n 16 -o log/%J.log mpirun python mpi_reply.py -b $1 -e $2 -r $3 -s $4 -c $5"
+#ssh psana command &
+ssh psana "source /reg/g/psdm/etc/psconda.sh; cd Python/WFS_interface; bsub -q psfehq -n 16 -o log/%J.log mpirun python mpi_reply.py -b $1 -e $2 -r $3 -s $4 -c $5" &
